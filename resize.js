3fb781ceb88732314fcd855af761b9578756dba0
loovjo <jonathan.loov@gmail.com>
2018-03-29 17:38:42 +0200

    Made changes to resize.js script

--- resize.js
+++ resize.js
@@ -1,24 +1,51 @@
 var ImageResize = require('node-image-resize'),
     fs = require('fs');
- 
+var spawn = require('child_process').spawn;
+
 var im = require('imagemagick');
 
-for (var i = 400; i < 431; i++) {
-    (function(){
-        var file1 = 'data/'+i + ".png";
-        var file2 = 'compressed/'+i + ".png";
-        im.resize({
-          srcPath: file1,
-          dstPath: file2,
-          width: 32,
-          height: 32
-        }, function(err, stdout, stderr){
-          //im.convert([file2, '-crop', '32x32+64+0',file2],function(err, stdout, stderr){
-              // foo 
-            //});
-            im.convert([file2, '-crop', '32x32','-gravity', 'center', '-background', 'black','-extent','32x32',file2],function(err, stdout, stderr){
-              // foo 
+
+fs.readdir("data", (err, files) => {
+    if (err) console.log("Error:", err);
+
+    var file_idx = 0;
+
+    files.forEach(file => {
+        if (file.endsWith(".png")) {
+
+            while (fs.existsSync("compressed/" + file_idx))
+                file_idx += 1;
+
+            let file1 = 'data/' + file;
+            let file2 = 'compressed/' + file_idx + ".png";
+
+            console.log(file1, "->", file2);
+
+            // Resize image
+            im.resize({
+              srcPath: file1,
+              dstPath: file2,
+              width: 32,
+              height: 32
+            }, function(err, stdout, stderr){
+                if (err) console.log("Error:", err);
+                // im.convert([file2, '-crop', '32x32+64+0',file2],function(err, stdout, stderr){
+                // });
+
+                im.convert([file2, '-crop', '32x32','-gravity', 'center', '-background', 'black','-extent','32x32',file2],function(err, stdout, stderr){
+                    if (err)
+                        console.log(err);
+                });
             });
-        });
-    })();
-}
+
+            // Copy coords
+            var coords1 = 'data/' + file.replace(/\.png/, "");
+            var coords2 = 'compressed/' + file_idx;
+
+            console.log(coords1, "->", coords2);
+            fs.createReadStream(coords1).pipe(fs.createWriteStream(coords2));
+
+        }
+    })
+
+})
