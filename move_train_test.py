9c67e90cc1cd81a6ca0f372271ef67c7c4ed4140
loovjo <jonathan.loov@gmail.com>
2018-04-09 09:32:32 +0200

    Made move_train_test use the right path

--- move_train_test.py
+++ move_train_test.py
@@ -4,6 +4,9 @@ import os
 
 TRAIN_TEST_SPLIT = 0.1
 
+TRAIN_IDX = 0
+TEST_IDX = 0
+
 for f in os.listdir(os.path.join("data", "tmp")):
     if not f.endswith(".png"):
         continue
@@ -14,10 +17,17 @@ for f in os.listdir(os.path.join("data", "tmp")):
         print(f, "failed")
         continue
 
+    train = random.random() > TRAIN_TEST_SPLIT
 
-    out = "train" if random.random() > TRAIN_TEST_SPLIT else "test"
+    out = "train" if train else "test"
     out = os.path.join("data", out)
 
-    os.rename(os.path.join("data", "tmp", f), os.path.join(out, f))
-    os.rename(os.path.join("data", "tmp", f + ".png"), os.path.join(out, f + ".png"))
+    while os.path.isfile(os.path.join(out, str(TRAIN_IDX if train else TEST_IDX))):
+        if train:
+            TRAIN_IDX += 1
+        else:
+            TEST_IDX += 1
+
+    os.rename(os.path.join("data", "tmp", f), os.path.join(out, str(TRAIN_IDX if train else TEST_IDX)))
+    os.rename(os.path.join("data", "tmp", f + ".png"), os.path.join(out, str(TRAIN_IDX if train else TEST_IDX) + ".png"))
 
